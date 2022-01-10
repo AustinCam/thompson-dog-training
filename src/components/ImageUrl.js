8469241a90b5edbafa85d261f502bcdd7097402a
AustinCam <austinmkpc@gmail.com>
2022-01-10 09:58:21 -0500

    image-url update

--- src/components/ImageUrl.js
+++ src/components/ImageUrl.js
@@ -2,6 +2,6 @@ import urlBuilder from "@sanity/image-url";
 
 export const urlFor = (source) =>
   urlBuilder({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-    dataset: process.env.REACT_APP_SANITY_DATASET_ID,
+    projectId: "e7vipft2",
+    dataset: "production",
   }).image(source);
