8f1da0382faaaf849c37986eca5620244a10b85f
AustinCam <austinmkpc@gmail.com>
2022-01-10 08:54:31 -0500

    client updates

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-  dataset: process.env.REACT_APP_SANITY_DATASET_ID,
+  projectId: "e7vipft2",
+  dataset: "production",
   apiVersion: "2022-01-10", // use current UTC date - see "specifying API version"!
   // token: "sanity-auth-token", // or leave blank for unauthenticated usage
   useCdn: true, // `false` if you want to ensure fresh data
